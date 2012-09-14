1472e499c7a0c0031aff8d9513adbfefe176ec41
georgeG <georgkam@gmail.com>
2012-09-14 15:47:22 +0300

    remove example code from this file

--- lib/bio-cd-hit-report.rb
+++ lib/bio-cd-hit-report.rb
@@ -1,14 +1,1 @@
 require_relative "bio-cd-hit-report/cd-hit-report"
-
-#cluster_file = "#{ENV['HOME']}/DBL_tags/cluster/cd-hit/cluster95.clstr"
-
-#report = Bio::CdHitReport.new(cluster_file)
-#puts report.max_members #print the max number of sequences in a cluster for the entire dataset
-#puts report.min_members #print the minimum number of sequences in a cluster for the entire dataset
-#puts report.total_clusters #print total number of clusters in the report
-
-#report.each_cluster do |c|
-  #puts "#{c.name} - #{c.members}" #list cluster name/id with respective number of sequences in the cluster
-#end
-
-
