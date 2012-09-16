7ea6dff8defb12ea6a28aa5fc636803dcf8110b9
georgeG <georgkam@gmail.com>
2012-09-16 12:18:06 +0300

    getting cluster members given cluster id/name

--- lib/bio-cd-hit-report/cd-hit-report.rb
+++ lib/bio-cd-hit-report/cd-hit-report.rb
@@ -1,6 +1,6 @@
 module Bio
 
-require_relative 'cluster.rb'
+  require_relative 'cluster.rb'
 
   class CdHitReport
 
@@ -14,6 +14,10 @@ require_relative 'cluster.rb'
 
     def total_clusters
       cluster_objs.size
+    end
+
+    def get_cluster(name)
+      cluster_objs.select{|cluster| cluster.name == name.to_s}.pop.members
     end
 
     def max_members
