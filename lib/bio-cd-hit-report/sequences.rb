81d87390ca169a28129525fc7c1f326e083bbfc2
georgeG <georgkam@gmail.com>
2013-03-06 23:48:40 +0300

    remove sequences file

--- lib/bio-cd-hit-report/sequences.rb
+++
@@ -1,7 +0,0 @@
-#class SeqData
-  #def initialize(fasta_file)
-    #Bio::FlatFile.auto(fasta_file).map{ |f| f}
-    #lines = File.readlines(names_file).map {|line| line.chomp!}
-    #puts lines.map {|line| entries.select {|entry| entry.definition == line }}
-  #end
-#end
