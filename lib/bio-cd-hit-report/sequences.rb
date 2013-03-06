0a1b47ceb124f71675c3d7a39b8ddd194d85b680
georgeG <georgkam@gmail.com>
2013-03-06 23:02:53 +0300

    comment out

---
+++ lib/bio-cd-hit-report/sequences.rb
@@ -0,0 +1,7 @@
+#class SeqData
+  #def initialize(fasta_file)
+    #Bio::FlatFile.auto(fasta_file).map{ |f| f}
+    #lines = File.readlines(names_file).map {|line| line.chomp!}
+    #puts lines.map {|line| entries.select {|entry| entry.definition == line }}
+  #end
+#end
