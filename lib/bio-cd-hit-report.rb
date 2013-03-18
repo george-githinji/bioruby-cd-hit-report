4b2c79c1ec9ed3a178c16691b57f0b9220c6d739
georgeG <georgkam@gmail.com>
2013-03-18 14:10:56 +0300

    clean the file

--- lib/bio-cd-hit-report.rb
+++ lib/bio-cd-hit-report.rb
@@ -1,11 +1,1 @@
 require_relative 'bio-cd-hit-report/cd-hit-report'
-
-#report = Bio::CdHitReport.new('bin/test.clstr')
-##report.report_file = 'bin/test.clstr'
-#report.parse.each do |cluster|
-  #puts cluster.id
-  ##puts cluster.members
-  #puts cluster.rep_seq
-#end
-
-#puts report.parse.length
